a39d6c62a0405dfd734d0f3b745483b63846041c
Luis Martin Schick <luis3720@hotmail.com>
2022-07-06 22:41:22 +0200

    Added faculty

---
+++ Faculty/Program.cs
@@ -0,0 +1,9 @@
+﻿double i, fact = 1, number;
+Console.Write("Enter any Number: ");
+number = int.Parse(Console.ReadLine());
+for (i = 1; i <= number; i++)
+{
+    fact = fact * i;
+}
+
+Console.Write("Factorial of " + number + " is: " + fact);
